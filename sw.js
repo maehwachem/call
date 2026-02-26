93c26dd3bc960407ab610f0b6a40b06e3fc53039
maehwachem <97303223+maehwachem@users.noreply.github.com>
2026-02-26 13:26:08 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,27 @@
-const CACHE_NAME = "attend-pwa-v1";
+const CACHE_NAME = "attend-pwa-v3"; // ← 숫자 올리기(중요)
 const ASSETS = [
+  "/call/?home=1",
   "/call/",
   "/call/index.html",
   "/call/manifest.webmanifest",
   "/call/icon-192.png",
-  "/call/icon-512.png"
+  "/call/icon-512.png",
+  "/call/sw.js"
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
+    Promise.all([
+      caches.keys().then((keys) =>
+        Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+      ),
+      self.clients.claim()
+    ])
   );
 });
 
